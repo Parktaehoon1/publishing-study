f8d74295af89782ef54d504fc227893226246b9a
park tae hoon <92parkth@gmail.com>
2023-02-07 18:44:59 +0900

    기억이 안남ㅆ음

--- backround/admin/js/menu.js
+++ backround/admin/js/menu.js
@@ -1,60 +1,61 @@
 window.onload = function () {
-	//     let img = $(".wrap1");
-  //     console.log(img);
+	    let img = $(".wrap1");
+      console.log(img);
 
-	// img.on("mousemove", function (event) {
-	// 	  console.log(event.pageY)
-  //       var styles = {
-  //         "-webkit-transform":
-  //           "translate3d(" +
-  //           event.pageX / -64 +
-  //           "px," +
-  //           event.pageY / -100 +
-  //           "px,0)",
-  //         "-moz-transform":
-  //           "translate3d(" +
-  //           event.pageX / -64 +
-  //           "px," +
-  //           event.pageY / -100 +
-  //           "px,0)",
-  //         "-o-transform":
-  //           "translate3d(" +
-  //           event.pageX / -64 +
-  //           "px," +
-  //           event.pageY / -100 +
-  //           "px,0)",
-  //         transform:
-  //           "translate3d(" +
-  //           event.pageX / -64 +
-  //           "px," +
-  //           event.pageY / -100 +
-  //           "px,0)",
-  //       };
+	img.on("mousemove", function (event) {
+    console.log(event.pageY,"Y")
+    console.log(event.pageX,"X")
+        var styles = {
+          "-webkit-transform":
+            "translate3d(" +
+            event.pageX / -64 +
+            "px," +
+            event.pageY / -100 +
+            "px,0)",
+          "-moz-transform":
+            "translate3d(" +
+            event.pageX / -64 +
+            "px," +
+            event.pageY / -100 +
+            "px,0)",
+          "-o-transform":
+            "translate3d(" +
+            event.pageX / -64 +
+            "px," +
+            event.pageY / -100 +
+            "px,0)",
+          transform:
+            "translate3d(" +
+            event.pageX / -64 +
+            "px," +
+            event.pageY / -100 +
+            "px,0)",
+        };
 
-  //       img.css(styles);
-  //     });
+        img.css(styles);
+      });
 
-  //     window.ondeviceorientation = function (event) {
-  //       var alpha = Math.round(event.alpha),
-  //         beta = Math.round(event.beta),
-  //         gamma = Math.round(event.gamma),
-  //         phoneStyles = {
-  //           "webkit-transform":
-  //             "translate3d(" + beta / 4 + "px," + gamma / 4 + "px,0)",
-  //           "-moz-transform":
-  //             "translate3d(" + beta / 4 + "px," + gamma / 4 + "px,0)",
-  //           "-o-transform":
-  //             "translate3d(" + beta / 4 + "px," + gamma / 4 + "px,0)",
-  //           "transform:":
-  //             "translate3d(" + beta / 4 + "px," + gamma / 4 + "px,0)",
-  //         };
+      window.ondeviceorientation = function (event) {
+        var alpha = Math.round(event.alpha),
+          beta = Math.round(event.beta),
+          gamma = Math.round(event.gamma),
+          phoneStyles = {
+            "webkit-transform":
+              "translate3d(" + beta / 4 + "px," + gamma / 4 + "px,0)",
+            "-moz-transform":
+              "translate3d(" + beta / 4 + "px," + gamma / 4 + "px,0)",
+            "-o-transform":
+              "translate3d(" + beta / 4 + "px," + gamma / 4 + "px,0)",
+            "transform:":
+              "translate3d(" + beta / 4 + "px," + gamma / 4 + "px,0)",
+          };
 
-  //       img.css(phoneStyles);
-  //     };
-  $(".wrap1").mousemove(function (e) {
-      console.log('sss')
-      var moveX = (e.pageX * -1) / 15;
-      var moveY = (e.pageY * -1) / 15;
-      $(this).css("background-position", moveX + "px " + moveY + "px");
-    });
+        img.css(phoneStyles);
+      };
+  // $(".wrap1").mousemove(function (e) {
+  //     console.log('sss')
+  //     var moveX = (e.pageX * -1) / 15;
+  //     var moveY = (e.pageY * -1) / 15;
+  //     $(this).css("background-position", moveX + "px " + moveY + "px");
+  //   });
 }
